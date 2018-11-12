221abdae1c8efa786615cc6198a19f3ac7fda935
Erick Diaz <erick@yours.co>
2018-11-11 21:17:23 -0700

    Finish Adaption first draft
    - Finished adaption calculations/output for all possible combinations
    of input into the adaption table in time for the demo on 11/16.
    
    - TODO
      * All the validation
      * Required guards in place
      * To to DRY-ify stuff; lots of repeat code
    
    - Pretty happy with the progres so far, and this works as a pretty good
    MVP

--- src/components/Adaption.vue
+++ src/components/Adaption.vue
@@ -23,8 +23,8 @@
         </td>
       </tr>
     </table>
-    <button @click="genParagraph('adaption')">Generate Adaption Paragraph</button>
-    <p v-if="adaptionParagraph">{{ adaptionParagraph }}</p>
+    <button @click="genParagraph()">Generate Adaption Paragraph</button>
+    <p class="paragraph" v-if="adaptionParagraph">{{ adaptionParagraph }}</p>
   </div>
 </template>
 
@@ -39,7 +39,8 @@ import {
   supportType,
   mediaTypeMapping,
   hisHer, 
-  heShe
+  heShe,
+  saveScore
 } from "@/utils";
 
 export default {
@@ -47,10 +48,24 @@ export default {
     startCase,
     convertPoa,
     inputType,
+    saveScore,
     genTotalScoreSentence() {
       const { sex, adaptionFrequencyAvg, firstName } = this;
-      if(adaptionFrequencyAvg == 0) {
-        return `In the area of adaption, ${firstName} presented with constrictions the ability to vary ${hisHer(sex)} musical-play in response to the therapist. (Note: Adaption to play examines how ${firstName} follows what the therapist is doing as a play partner, but not necessarily how they respond to specific musical elements).`;
+      switch(Math.floor(adaptionFrequencyAvg)) {
+        case 0: {
+          return `In the area of adaption, ${firstName} presented with constrictions the ability to vary ${hisHer(sex)} musical-play in response to the therapist. (Note: Adaption to play examines how ${firstName} follows what the therapist is doing as a play partner, but not necessarily how they respond to specific musical elements).`;
+        }
+        case 1:
+        case 2: {
+          return `In the area of adaption, ${firstName} occasionally displayed the ability to vary ${hisHer(sex)} musical-play in response to the therapist. (Note: Adaption to play examines how ${firstName} follows what the therapist is doing as a play partner, but not necessarily how they respond to specific musical elements).`;
+        }
+        case 3:
+        case 4: {
+          return `In the area of adaption, ${firstName} often displayed the ability to vary ${hisHer(sex)} musical-play in response to the therapist. (Note: Adaption to play examines how ${firstName} follows what the therapist is doing as a play partner, but not necessarily how they respond to specific musical elements).`;
+        }
+        case 5: {
+          return `In the area of adaption, ${firstName} consistently displayed the ability to vary ${hisHer(sex)} musical-play in response to the therapist. (Note: Adaption to play examines how ${firstName} follows what the therapist is doing as a play partner, but not necessarily how they respond to specific musical elements).`;
+        }
       }
     },
     genJoinsSentence() {
@@ -61,8 +76,10 @@ export default {
         adaptionMedia,
         firstName,
       } = this;
-      if(adaptionFrequency.joins == 0) {
-        return `${firstName} appeared to display challenges joining into ${mediaTypeMapping(adaptionMedia.joins)} play as led by therapists ${adaptionPOA.joins} ${adaptionSupportSentence()}.`;
+      switch(adaptionFrequency.joins) {
+        case 0: {
+          return `${firstName} appeared to display challenges joining into ${mediaTypeMapping(adaptionMedia.joins)} play as led by therapist's ${adaptionPOA.joins} ${adaptionSupportSentence()}.`;
+        }
       }
     },
     genAdjustsSentence() {
@@ -73,8 +90,25 @@ export default {
         adaptionPOA,
         adaptionSupportSentence,
       } = this;
-      if (adaptionFrequency.adjusts == 0) {
-        return `${_.capitalize(heShe(sex))} seemed to display challenges adapting ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapists ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+      switch(adaptionFrequency.adjusts) {
+        case 0: {
+          return `${_.capitalize(heShe(sex))} seemed to display challenges adapting ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
+        case 1: {
+          return `${_.capitalize(heShe(sex))} rarely adapted ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
+        case 2: {
+          return `${_.capitalize(heShe(sex))} occasionally adapted ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
+        case 3: {
+          return `${_.capitalize(heShe(sex))} frequently adapted ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
+        case 4: {
+          return `${_.capitalize(heShe(sex))} often adapted ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
+        case 5: {
+          return `${_.capitalize(heShe(sex))} consistently adapted ${hisHer(sex)} participation in ${mediaTypeMapping(adaptionMedia.joins)} play as lead by therapist's ${adaptionPOA.adjusts} ${adaptionSupportSentence()}.`;
+        }
       }
     },
     genTurnsSentence() {
@@ -85,12 +119,30 @@ export default {
         adaptionPOA,
         firstName
       } = this;
-      if (adaptionFrequency.turns == 0) {
-        const firstClause = adaptionSupport.turns == 5 
-          ? supportType(adaptionSupport.turns)
-          : `When given ${supportType(adaptionSupport.turns)}`;
 
+      const firstClause = adaptionSupport.turns == 5
+        ? supportType(adaptionSupport.turns)
+        : `When given ${supportType(adaptionSupport.turns)}`;
+
+      switch(adaptionFrequency.turns) {
+        case 0: {
           return `${firstClause}, ${firstName} appeared to display difficulties engaging in ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
+        case 1: {
+          return `${firstClause}, ${firstName} rarely illustrated the ability to enage in  ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
+        case 2: {
+          return `${firstClause}, ${firstName} occasionally illustrated the ability to enage in  ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
+        case 3: {
+          return `${firstClause}, ${firstName} frequently illustrated the ability to enage in  ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
+        case 4: {
+          return `${firstClause}, ${firstName} often illustrated the ability to enage in  ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
+        case 5: {
+          return `${firstClause}, ${firstName} consistently illustrated the ability to enage in  ${mediaTypeMapping(adaptionMedia.turns)} turn-taking in response to the therapist's ${adaptionPOA.turns}.`;
+        }
       }
     },
     genStopsSentence() {
@@ -102,8 +154,25 @@ export default {
         firstName,
         sex
       } = this;
-      if (adaptionFrequency.turns == 0) {
-        return `Lastly, ${firstName} seemed to display challenges stopping ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+      switch(adaptionFrequency.stops) {
+        case 0: {
+          return `Lastly, ${firstName} seemed to display challenges stopping ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
+        case 1: {
+          return `Lastly, ${firstName} rarely exhibited the capacity to stop ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
+        case 2: {
+          return `Lastly, ${firstName} occasionally exhibited the capacity to stop ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
+        case 3: {
+          return `Lastly, ${firstName} frequently exhibited the capacity to stop ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
+        case 4: {
+          return `Lastly, ${firstName} often exhibited the capacity to stop ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
+        case 5: {
+          return `Lastly, ${firstName} consistently exhibited the capacity to stop ${hisHer(sex)} ${mediaTypeMapping(adaptionMedia.stops)} play when the therapist stopped the music ${adaptionSupportSentence(adaptionSupport.stops)}`;
+        }
       }
     },
     adaptionSupportSentence() {
@@ -112,27 +181,24 @@ export default {
         ? `while being provided with ${supportType(adaptionSupport.joins)}`
         : supportType(adaptionSupport.joins);
     },
-    genParagraph(type) {
-      switch(type) {
-        case "adaption": {
-          this.$store.commit("adaption/setParagraph", _.join(
-            [
-              this.genTotalScoreSentence(type),
-              this.genJoinsSentence(),
-              this.genAdjustsSentence(),
-              this.genTurnsSentence(),
-              this.genStopsSentence(),
-            ],
-            " "
-          ));
-        }
-      }
+    genParagraph() {
+      this.$store.commit("adaption/setParagraph", _.join(
+        [
+          this.genTotalScoreSentence(),
+          this.genJoinsSentence(),
+          this.genAdjustsSentence(),
+          this.genTurnsSentence(),
+          this.genStopsSentence(),
+        ],
+        " "
+      ));
     },
   },
   computed: {
     ...mapState({
       sex: state => state.sex,
       measures: state => state.measures,
+      firstName: state => state.firstName,
       adaptionAttributes: state => _.keys(state.adaption.frequency),
       adaptionFrequency: state => state.adaption.frequency,
       adaptionSupport: state => state.adaption.support,
@@ -150,5 +216,10 @@ export default {
 <style scoped>
 table {
   margin: auto;
+}
+
+.paragraph {
+  text-align: left;
+  font-size: 1.1em;
 }
 </style>
